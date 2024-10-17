6a28eae0995102d4b54d85529655b1c619b9cb79
StellarSatcker2 <csbs269058@saranathan.ac.in>
2024-10-17 11:11:02 +0530

    pgm : added updated programs

--- BrainStorms/Throw_throws.java
+++ BrainStorms/Throw_throws.java
@@ -1,11 +1,16 @@
 //program to show the the working flow of throw and throws
-
+import java.util.Scanner;
 class NoMatchException extends Exception{
 	public NoMatchException(String message){
 		super(message);
 	}
 }
 class Main{
+	Scanner sc;
+	public Main(){
+		sc=new Scanner(System.in);
+		System.out.println("Constructor of the main class initialized");
+	}
 	
 	void check(String s) throws NoMatchException{
 		if(s=="CSBS"){
@@ -17,8 +22,10 @@ class Main{
 		
 	public static void main(String...a){
 		Main ob=new Main();
+		System.out.println("Enter any String : ");
+		string s=sc.nextLine();
 		try{
-			ob.check("CSBS");
+			ob.check(string);
 		}catch(NoMatchException e){
 			System.out.println(e.getMessage());
 		}
